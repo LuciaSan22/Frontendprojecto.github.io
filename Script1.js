4aef86ca8b6faa178a8d55348840767344e66d35
LuciaSan22 <126192027+LuciaSan22@users.noreply.github.com>
2023-02-23 14:41:36 -0300

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,6 @@
+// JavaScript source codigo
+
+document.getElementById("boton").addEventListener("click", function () {
+    console.log("informacion de contacto");
+    document.getElementById("demo").innerHTML = "E-mail:nellie.austin@example.com"
+});
